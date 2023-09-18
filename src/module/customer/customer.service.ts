a260e933434ab9ea613b9dc0cb2f98fcc2c68cc8
unknown <rockph0029@gmail.com>
2023-09-18 12:54:17 +0800

    changes

--- src/module/customer/customer.service.ts
+++ src/module/customer/customer.service.ts
@@ -20,6 +20,18 @@ export class CustomerService {
 
   // Find customer's total bought in volume
   findCustomerVolume() {
-  return  this.customerRepository.createQueryBuilder('customer').select([``])
+    return this.customerRepository
+      .createQueryBuilder('customer')
+      .select([
+        `customer."customer_id"`,
+        `customer."customer_name"`,
+        `sum(sales_items.quantity * products.packaging_size) as total_bought`,
+      ])
+      .leftJoin('customer.sales', 'product_sales')
+      .innerJoin('product_sales.sales_items', 'sales_items')
+      .innerJoin('sales_items.product', 'products')
+      .groupBy('customer.customer_id')
+      .orderBy('total_bought', 'DESC')
+      .getRawMany();
   }
 }

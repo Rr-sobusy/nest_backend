cf6d8c4e29c4bfb7c000f9811a5f9a4bffe315d0
unknown <rockph0029@gmail.com>
2023-10-02 16:52:23 +0800

    changes

--- src/module/packaging/packaging.service.ts
+++ src/module/packaging/packaging.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PackagingEntities } from 'src/entitities/packaging.entities';
+
 
 @Injectable()
-export class PackagingService {}
+export class PackagingService {
+    constructor(){}
+}
